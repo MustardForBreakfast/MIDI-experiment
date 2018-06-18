3c5d40d1e51749640fcdb63a785d1416aefa6054
Justin Wayne <wayne.jch@gmail.com>
2018-06-18 00:45:17 -0500

    renamed file, organized comments

--- src/index.js
+++ src/index.js
@@ -2,83 +2,130 @@ import testSong from './midi/SuperMarioBrothers.midi';
 import { initializeOrchestra } from './midiHelpers.js';
 
 /*
-  List of available instruments:
+List of Available instruments:
+(Master: https://github.com/gleitz/midi-js-soundfonts/tree/gh-pages/FluidR3_GM)
 
-  accordion
+// Strings
   acoustic_bass
-  acoustic_grand_piano
   acoustic_guitar_nylon
   acoustic_guitar_steel
-  agogo
-  alto_sax
-  applause
-  bagpipe
   banjo
-  baritone_sax
-  bassoon
-  bird_tweet
-  blown_bottle
-  brass_section
-  breath_noise
-  bright_acoustic_piano
-  celesta
   cello
-  choir_aahs
-  church_organ
-  clarinet
-  clavinet
   contrabass
   distortion_guitar
-  drawbar_organ
   dulcimer
   electric_bass_finger
   electric_bass_pick
-  electric_grand_piano
   electric_guitar_clean
   electric_guitar_jazz
   electric_guitar_muted
-  electric_piano_1
-  electric_piano_2
-  english_horn
   fiddle
-  flute
-  french_horn
   fretless_bass
-  fx_1_rain
-  fx_2_soundtrack
-  fx_3_crystal
-  fx_4_atmosphere
-  fx_5_brightness
-  fx_6_goblins
-  fx_7_echoes
-  fx_8_scifi
-  glockenspiel
   guitar_fret_noise
   guitar_harmonics
-  gunshot
-  harmonica
-  harpsichord
-  helicopter
-  honkytonk_piano
-  kalimba
   koto
+  orchestral_harp
+  overdriven_guitar
+  pizzicato_strings
+  shamisen
+  sitar
+  slap_bass_1
+  slap_bass_2
+  string_ensemble_1
+  string_ensemble_2
+  tremolo_strings
+  viola
+  violin
+
+
+// Woodwinds
+  accordion
+  alto_sax
+  bagpipe
+  baritone_sax
+  bassoon
+  clarinet
+  flute
+  harmonica
+  oboe
+  ocarina
+  pan_flute
+  piccolo
+  recorder
+  shanai
+  soprano_sax
+  tango_accordion
+  tenor_sax
+
+
+// Brass
+  brass_section
+  english_horn
+  french_horn
+  muted_trumpet
+  trombone
+  trumpet
+  tuba
+
+
+// Pianos and Keyboards
+  acoustic_grand_piano
+  bright_acoustic_piano
+  celesta
+  clavinet
+  electric_grand_piano
+  electric_piano_1
+  electric_piano_2
+  honkytonk_piano
+  harpsichord
+
+
+// Pipes
+  church_organ
+  drawbar_organ
+  lead_3_calliope
+  percussive_organ
+  reed_organ
+  rock_organ
+  shakuhachi
+
+
+// Vocal
+  choir_aahs
+  voice_oohs
+  whistle
+
+
+// Bells and Mallets
+  glockenspiel
+  kalimba
+  marimba
+  music_box
+  tinkle_bell
+  tubular_bells
+  vibraphone
+  xylophone
+
+
+// Percussion
+  agogo
+  melodic_tom
+  percussion // ERROR with this one.
+  steel_drums
+  synth_drum
+  taiko_drum
+  timpani
+  woodblock
+
+
+// Synth and Electronic
   lead_1_square
   lead_2_sawtooth
-  lead_3_calliope
   lead_4_chiff
   lead_5_charang
   lead_6_voice
   lead_7_fifths
-  lead_8_bass_lead
-  marimba
-  melodic_tom
-  music_box
-  muted_trumpet
-  oboe
-  ocarina
-  orchestra_hit
-  orchestral_harp
-  overdriven_guitar
+  lead_8_bass_lead // ERROR with this one
   pad_1_new_age
   pad_2_warm
   pad_3_polysynth
@@ -87,60 +134,43 @@ import { initializeOrchestra } from './midiHelpers.js';
   pad_6_metalic
   pad_7_halo
   pad_8_sweep
-  pan_flute
-  percussion // ERROR with this one.
-  percussive_organ
-  piccolo
-  pizzicato_strings
-  recorder
-  reed_organ
-  reverse_cymbal
-  rock_organ
-  seashore
-  shakuhachi
-  shamisen
-  shanai
-  sitar
-  slap_bass_1
-  slap_bass_2
-  soprano_sax
-  steel_drums
-  string_ensemble_1
-  string_ensemble_2
   synth_bass_1
   synth_bass_2
   synth_brass_1
   synth_brass_2
   synth_choir
-  synth_drum
   synth_strings_1
   synth_strings_2
-  taiko_drum
-  tango_accordion
+
+// SFX
+  applause
+  bird_tweet
+  blown_bottle
+  breath_noise
+  fx_1_rain
+  fx_2_soundtrack
+  fx_3_crystal
+  fx_4_atmosphere
+  fx_5_brightness
+  fx_6_goblins
+  fx_7_echoes
+  fx_8_scifi
+  gunshot
+  helicopter
+  orchestra_hit
+  reverse_cymbal
+  seashore
   telephone_ring
-  tenor_sax
-  timpani
-  tinkle_bell
-  tremolo_strings
-  trombone
-  trumpet
-  tuba
-  tubular_bells
-  vibraphone
-  viola
-  violin
-  voice_oohs
-  whistle
-  woodblock
-  xylophone
 */
+
 
 const trackAssignments = {
   // TRACK 1 IS RESERVED FOR METADATA. Start at 2.
+  // All empty tracks will not play.
   2: 'steel_drums',
   3: 'steel_drums',
   4: 'tuba',
-  5: 'synth_drum',
+  // 5: 'synth_drum',
 }
 
 const PlayerPromise = initializeOrchestra(trackAssignments, testSong);
